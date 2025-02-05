9d79fddd00c5d7fbad1950a22128468e5d5c9745
ax_pokl <ax_pokl@qq.com>
2025-02-05 15:41:16 +0800

    fix

--- diandeng10_a_faster2_png.pas
+++ diandeng10_a_faster2_png.pas
@@ -7,10 +7,10 @@ uses display;
 
 const m=1000;
 
-type TMat=array[-1..m+1,-1..m+1]of Boolean;
+type TMat=array[-1..m,-1..m]of Boolean;
 
 var n:longword;
-var l,a:TMat;
+var a,l,r,t:TMat;
 var i,j,k:longint;
 
 {$ifdef disp}
@@ -44,8 +44,6 @@ SetBB(bb);
 FreshWin();
 end;
 
-procedure DrawMat();begin DrawMat(l);end;
-
 procedure PrintMat(s:ansistring;mat:TMAT);
 begin
 DrawMat(mat);
@@ -54,33 +52,38 @@ DrawBMP(_pmain,b,0,0,n,n,0,0,n,n);
 SaveBMP(b,'png'+s+'/'+i2s(n)+'.png');
 ReleaseBMP(b);
 end;
-
-procedure PrintMat(s:ansistring);begin PrintMat(s,l);end;
 {$endif}
 
 procedure MakeMat();
 begin
-for i:=0 to n-1 do
-  l[n-1,i]:=False;
+for i:=0 to n-1 do l[n-1,i]:=False;
 l[n,0]:=False;
 for j:=n-2 downto -1 do
   for i:=0 to n-1 do
-    l[j,i]:=not((l[j+1,i] xor l[j+1,i+1]) xor (l[j+1,i-1] xor l[j+2,i]));
-for i:=0 to n-1 do
-  l[n,i]:=False;
+    begin
+    l[j,i]:=not(l[j+1,i] xor l[j+1,i+1] xor l[j+1,i-1] xor l[j+2,i]);
+    //l[j,i]:=l[j,i] xor a[j+1,i]
+    end;
+
+for i:=0 to n-1 do l[n,i]:=False;
 l[n,0]:=True;
 for j:=n-1 downto 0 do
   for i:=0 to n-1 do
-    l[j,i]:=(l[j+1,i] xor l[j+1,i+1]) xor (l[j+1,i-1] xor l[j+2,i]);
+    begin
+    l[j,i]:=(l[j+1,i] xor l[j+1,i+1] xor l[j+1,i-1]);
+    if j<n-1 then l[j,i]:=l[j,i] xor l[j+2,i];
+    end;
+
 for j:=1 to n-1 do
   for i:=0 to n-1 do
     begin
     l[j,i]:=(l[j-1,i+1] xor l[j-1,i-1]);
     if j>1 then l[j,i]:=l[j,i] xor l[j-2,i];
     end;
-for j:=0 to n-1 do if l[-1,j] then l[j,-1]:=True;
+
+for i:=0 to n-1 do l[i,-1]:=l[-1,i];
 for i:=0 to n-1 do l[-1,i]:=False;
-for i:=0 to n-1 do for j:=0 to n-1 do a[i,j]:=(i=j);
+for i:=0 to n-1 do for j:=0 to n-1 do r[i,j]:=(i=j);
 end;
 
 procedure CalcMat();
@@ -100,9 +103,9 @@ for k:=0 to n-1 do
         l[j,i]:=l[j,i] xor l[k,i];
         l[k,i]:=l[j,i] xor l[k,i];
         l[j,i]:=l[j,i] xor l[k,i];
-        a[j,i]:=a[j,i] xor a[k,i];
-        a[k,i]:=a[j,i] xor a[k,i];
-        a[j,i]:=a[j,i] xor a[k,i];
+        r[j,i]:=r[j,i] xor r[k,i];
+        r[k,i]:=r[j,i] xor r[k,i];
+        r[j,i]:=r[j,i] xor r[k,i];
         end;
     for j:=k+1 to n-1 do
       if l[j,k] then
@@ -110,10 +113,10 @@ for k:=0 to n-1 do
         for i:=0 to n-1 do //for a-1
           begin
           l[j,i]:=l[j,i] xor l[k,i];
-          a[j,i]:=a[j,i] xor a[k,i];
+          r[j,i]:=r[j,i] xor r[k,i];
           end;
         l[j,-1]:=l[j,-1] xor l[k,-1];
-        a[j,-1]:=a[j,-1] xor a[k,-1];
+        r[j,-1]:=r[j,-1] xor r[k,-1];
         end;
     end;
   end;
@@ -125,10 +128,10 @@ for i:=n-1 downto 0 do
         for k:=j to n-1 do
           begin
           l[j,k]:=l[j,k] xor l[i,k];
-          a[j,k]:=a[j,k] xor a[i,k];
+          r[j,k]:=r[j,k] xor r[i,k];
           end;
         l[j,-1]:=l[j,-1] xor l[i,-1];
-        a[j,-1]:=a[j,-1] xor a[i,-1];
+        r[j,-1]:=r[j,-1] xor r[i,-1];
         end;
 end;
 
@@ -136,12 +139,12 @@ procedure GeneMat();
 begin
 for i:=0 to n-1 do
   begin
-  l[0,i]:=l[i,-1];
+  t[0,i]:=l[i,-1];
   l[i,-1]:=false;
   end;
 for j:=1 to n-1 do
   for i:=0 to n-1 do
-    l[j,i]:=not(l[j-1,i-1] xor l[j-1,i] xor l[j-1,i+1] xor l[j-2,i]);
+    t[j,i]:=not(t[j-1,i-1] xor t[j-1,i] xor t[j-1,i+1] xor t[j-2,i]) xor a[j-1,i];
 end;
 
 begin
@@ -153,23 +156,11 @@ b:=CreateBMP(m,m);
 for n:=1 to m do
   begin
   write(n,#9);
-  write('m');MakeMat();
-  {$ifdef disp}
-  write('%');PrintMat('_A');
-  //readln();
-  {$endif}
-  write('c');CalcMat();
-  {$ifdef disp}
-  write('%');PrintMat('_E');
-  write('%');PrintMat('_A-1',a);
-  {$endif}
-  write('g');GeneMat();
-  //write('@');PrintMat();
-  {$ifdef disp}
-  write('%');PrintMat('');
+  write('m');MakeMat();{$ifdef disp}write('%');PrintMat('_A',l);{$endif}
+  write('c');CalcMat();{$ifdef disp}write('%');PrintMat('_E',l);write('%');PrintMat('_R',r);{$endif}
+  write('g');GeneMat();{$ifdef disp}write('%');PrintMat('_T',t);{$endif}
   write(#9,s,#9,n*n,#9,s/n/n:0:5);
-  if not(iswin()) then halt;
-  {$endif}
+  {$ifdef disp}if not(iswin()) then halt;{$endif}
   writeln();
   end;
 end.
