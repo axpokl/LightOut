a0c45704c147c6babcf10d4d2e32cc7d1e893491
Zheng Wang <ax_pokl@sina.com>
2026-09-08 16:17:42 +0800

    H70

--- all_results_append.bat
+++ all_results_append.bat
@@ -1,2 +1,2 @@
-for /L %%i in (63,1,69) do @(echo ===== diandeng10_b_faster2_png_H%%i.exe ===== & ".\diandeng10_b_faster2_png_H%%i.exe" & echo.) >> all_results_b.txt 2>&1
-for /L %%i in (63,1,69) do @(echo ===== diandeng10_a_faster2_png_H%%i.exe ===== & ".\diandeng10_a_faster2_png_H%%i.exe" & echo.) >> all_results_a.txt 2>&1
+for /L %%i in (63,1,70) do @(echo ===== diandeng10_b_faster2_png_H%%i.exe ===== & ".\diandeng10_b_faster2_png_H%%i.exe" & echo.) >> all_results_b.txt 2>&1
+for /L %%i in (63,1,70) do @(echo ===== diandeng10_a_faster2_png_H%%i.exe ===== & ".\diandeng10_a_faster2_png_H%%i.exe" & echo.) >> all_results_a.txt 2>&1
